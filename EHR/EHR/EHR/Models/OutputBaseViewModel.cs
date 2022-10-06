ebf82ad800daceb8e78ca91e12ef3a30c9859aa2
renkaifei <18910891894@163.com>
2022-10-06 13:57:53 +0800

    新增响应结果格式

---
+++ EHR/EHR/EHR/Models/OutputBaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EHR.Models
+{
+    public class OutputBaseViewModel
+    {
+        public int Status { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ EHR/EHR/EHR/Models/ResultViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EHR.Models
+{
+    public class ResultViewModel<T>: OutputBaseViewModel
+    {
+        public T Data { get; set; }
+    }
+}
