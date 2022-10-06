30f1f85ac16810237749fc20f80044a0ae252ce1
renkaifei <18910891894@163.com>
2022-10-06 14:41:57 +0800

    新增公共类库

---
+++ EHR/EHR/EHRUtil/ExceptionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EHRUtil
+{
+    public static class ExceptionExtensions
+    {
+        public static string GetMessage(this Exception ex)
+        {
+            return ex.InnerException == null ? ex.Message : ex.InnerException.Message;
+        }
+    }
+}
