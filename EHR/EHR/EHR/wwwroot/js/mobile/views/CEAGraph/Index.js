a994d218e748eef95bd3b2b44980937ccb4ce709
renkaifei <18910891894@163.com>
2023-01-01 19:22:52 +0800

    新增CEA 模块

---
+++ EHR/EHR/EHR/wwwroot/js/mobile/views/CEAGraph/Index.js
@@ -0,0 +1,26 @@
+﻿let ceaGraphChartPanel;
+let patientCaseTumorMarkerService;
+
+$(function () {
+    initializeComponent();
+    pageLoad();
+});
+
+function initializeComponent() {
+    ceaGraphChartPanel = new CEAGraphChartPanel();
+    ceaGraphChartPanel.id = "pnlCEAGraph";
+    ceaGraphChartPanel.fit = true;
+
+}
+
+function pageLoad() {
+    var patientCaseId = 1;
+    patientCaseTumorMarkerService = new PatientCaseTumorMarkerService();
+    patientCaseTumorMarkerService.getListByPatientCaseId(patientCaseId)
+        .then(function (data) {
+            var arrceaData = data.filter(function (item) {
+                return item.tumorMarker.name == "CEA";
+            });
+            ceaGraphChartPanel.build(arrceaData);
+        });
+}
